148aca68876b2ac97d0eb17c3f0592d8b092d034
Bruno Wego <brunowego@gmail.com>
2022-03-10 16:00:03 -0300

    chore(nextjs): added react-hot-toast implementation

--- frameworks/nodejs/nextjs/with-toast/src/pages/_app.tsx
+++ frameworks/nodejs/nextjs/with-toast/src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Toaster position="bottom-right" />
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
