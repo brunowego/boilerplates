64e4315bc06855875f3c5dcaa62bd8daa0535cf1
Bruno Wego <brunowego@gmail.com>
2023-02-02 19:33:25 -0300

    chore(vite): added env to port listen

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 /**
  * @see https://vitejs.dev/config/
  */
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
   /**
    * @type { import('vite').UserConfig }
    */
   return {
+    server: {
+      host: '0.0.0.0',
+      port: parseInt(process.env.VITE_PORT),
+    },
     plugins: [react()],
   }
 })
