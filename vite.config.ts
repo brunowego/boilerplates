52b97c74a6a66b5d67b5a131375c8a4a25130783
Bruno Wego <brunowego@gmail.com>
2023-02-02 19:08:58 -0300

    chore(vite): added type

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// @see https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+/**
+ * @see https://vitejs.dev/config/
+ */
+export default defineConfig(() => {
+  /**
+   * @type { import('vite').UserConfig }
+   */
+  return {
+    plugins: [react()],
+  }
 })
