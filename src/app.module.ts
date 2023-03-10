b1ff9ac1b81824236a75e77a8b72821743219164
Bruno Wego <brunowego@gmail.com>
2023-03-10 10:44:17 -0300

    chore(common): prefer folder in plural

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import appConfig from './common/config/app.config'
-import { configValidationSchema as validationSchema } from './common/validation/config.validation'
+import appConfig from './common/configs/app.config'
+import { configValidationSchema as validationSchema } from './common/validations/config.validation'
 import { BeatModule } from './modules/beat/beat.module'
 import { HealthModule } from './modules/health/health.module'
 
