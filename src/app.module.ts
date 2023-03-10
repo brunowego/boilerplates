54b1465558a028a3dd5ba065df9e92d04b345104
Bruno Wego <brunowego@gmail.com>
2023-03-10 11:08:11 -0300

    chore(modules): move config to common folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from './modules/config/config.module'
+import { ConfigModule } from '@nestjs/config'
+import appConfig from './common/configs/app.config'
+import { configValidationSchema as validationSchema } from './common/validations/config.validation'
 import { BeatModule } from './modules/beat/beat.module'
 import { HealthModule } from './modules/health/health.module'
 import { UserModule } from './modules/user/user.module'
 import { PostModule } from './modules/post/post.module'
 
 @Module({
-  imports: [ConfigModule, BeatModule, HealthModule, UserModule, PostModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+      validationSchema,
+    }),
+    BeatModule,
+    HealthModule,
+    UserModule,
+    PostModule,
+  ],
 })
 export class AppModule {}
