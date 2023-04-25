0af66cbbe0f96e391e34a33eaf089090e30b5a36
Bruno Wego <brunowego@gmail.com>
2023-04-25 12:31:47 -0300

    chore(repo): final adjusts

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@nestjs/common'
 import { sendEmail } from '@acme/mailer'
 import { SignUpDto } from './signup.dto'
-import { nanoid } from 'nanoid'
+import { customAlphabet } from 'nanoid'
 
 @Injectable()
 export class AuthService {
   async signUp(signUpDto: SignUpDto): Promise<void> {
     // Register the user in the database
 
+    const nanoid = customAlphabet('1234567890ABCDEF', 6)
+
     await sendEmail('RegisterEmail', {
       to: signUpDto.email,
       subject: 'Welcome aboard!',
       props: {
         name: signUpDto.name,
-        code: nanoid(6).toUpperCase(),
+        code: nanoid(),
       },
     })
   }
