2860ce99bff2e183021f5aa0de3675b0401b258d
Bruno Wego <brunowego@gmail.com>
2023-10-22 15:51:01 -0300

    chore(api): added healthcheck module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { HealthcheckModule } from './modules/healthcheck/healthcheck.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HealthcheckModule],
 })
 export class AppModule {}
