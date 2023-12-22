5a7e1276f3bd36d070c50e884e71e84bae5248f9
Bruno Wego <brunowego@gmail.com>
2023-12-22 10:19:28 -0300

    chore(repo): first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Medusa Base
+
+TODO
+
+## Docs
+
+- [Developer Guide](/DEVELOPER.md)
---
+++ apps/backend/medusa-config.js
@@ -0,0 +1,59 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const plugins = [
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  {
+    resolve: `@medusajs/file-local`,
+    /** @type {import('@medusajs/file-local').PluginOptions} */
+    options: {
+      upload_dir: 'uploads',
+    },
+  },
+  {
+    resolve: '@medusajs/admin',
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: true,
+      develop: {
+        open: process.env.OPEN_BROWSER !== 'false',
+      },
+    },
+  },
+]
+
+const REDIS_URL = process.env.REDIS_URL
+
+const modules = {
+  eventBus: {
+    resolve: '@medusajs/event-bus-redis',
+    options: {
+      redisUrl: REDIS_URL,
+    },
+  },
+  cacheService: {
+    resolve: '@medusajs/cache-redis',
+    options: {
+      redisUrl: REDIS_URL,
+    },
+  },
+}
+
+/** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
+const projectConfig = {
+  jwtSecret: process.env.JWT_SECRET,
+  cookieSecret: process.env.COOKIE_SECRET,
+  store_cors: process.env.STORE_CORS,
+  database_url: process.env.DATABASE_URL,
+  admin_cors: process.env.ADMIN_CORS,
+  redis_url: REDIS_URL,
+}
+
+/** @type {import('@medusajs/medusa').ConfigModule} */
+module.exports = {
+  projectConfig,
+  plugins,
+  modules,
+}
