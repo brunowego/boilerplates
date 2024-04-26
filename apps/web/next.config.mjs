34177c935c98f7bb61cbaedbc9616379dece99f9
Bruno Wego <brunowego@gmail.com>
2024-04-26 13:49:11 -0300

    feat: add CORS headers for path wildcard

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,6 +2,26 @@
  * @type { import('next').NextConfig }
  */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Authorization,Content-Type,Date',
+          },
+        ],
+      },
+    ]
+  },
+
   transpilePackages: ['@acme/ui'],
 
   experimental: {
