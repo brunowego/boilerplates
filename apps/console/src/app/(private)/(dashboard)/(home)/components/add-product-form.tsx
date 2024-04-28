bafdf2288bde15408c3b10914cf097c42747a473
Bruno Wego <brunowego@gmail.com>
2024-04-28 11:08:35 -0300

    chore(repo): first commit

---
+++ apps/console/src/app/(private)/(dashboard)/(home)/components/add-product-form.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import type { HTMLAttributes, JSX } from 'react'
+
+import { type z, zodResolver } from '@acme/ui/lib/zod'
+import { useForm, type SubmitHandler } from '@acme/ui/hooks/use-form'
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from '@acme/ui/components/form'
+import Input from '@acme/ui/components/input'
+import Button from '@acme/ui/components/button'
+import { Loader2 } from '@acme/ui/components/icon'
+import cn from '@acme/ui/lib/cn'
+
+import { insertProductSchema } from '@/schemas'
+import HookFormDevtool from '@/components/hookform-devtool'
+
+const formSchema = insertProductSchema
+
+type FormValues = z.infer<typeof formSchema>
+
+interface AddProductFormProps extends HTMLAttributes<HTMLFormElement> {}
+
+export default function AddProductForm({
+  className,
+  ...props
+}: AddProductFormProps): JSX.Element {
+  const { register, formState, reset, handleSubmit, control, ...form } =
+    useForm<FormValues>({
+      mode: 'onChange',
+      defaultValues: {
+        title: '',
+        description: '',
+      },
+      resolver: zodResolver(formSchema),
+    })
+
+  const onSubmit: SubmitHandler<FormValues> = async (values: FormValues) => {
+    console.log(values)
+  }
+
+  return (
+    <>
+      <HookFormDevtool control={control} />
+
+      <Form {...{ register, formState, reset, handleSubmit, control, ...form }}>
+        <form
+          className={cn('grid space-y-2', className)}
+          onSubmit={handleSubmit(onSubmit)}
+          {...props}
+        >
+          <FormField
+            control={control}
+            name='title'
+            render={({ field }) => (
+              <FormItem className='max-w-md'>
+                <FormLabel>Title</FormLabel>
+
+                <FormControl>
+                  <Input className='h-12' type='text' {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className='flex'>
+            <Button
+              className='ml-auto'
+              disabled={!formState.isDirty || !formState.isValid}
+              size='lg'
+              type='submit'
+              variant='secondary'
+            >
+              {formState.isSubmitting ? (
+                <Loader2 className='size-5 animate-spin' />
+              ) : (
+                <span>Add</span>
+              )}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  )
+}
---
+++ packages/ui/src/components/index.ts
@@ -0,0 +1,5 @@
+export { LogoMark } from './logo'
+
+export { default as Button, buttonVariants } from './button'
+export { default as Icon } from './icon'
+export { default as TailwindIndicator } from './tailwind-indicator'
