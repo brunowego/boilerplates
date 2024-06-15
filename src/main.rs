b6e5be2348506498592fffc4caa91b00d2389d5e
Bruno Wego <brunowego@gmail.com>
2024-06-15 09:52:54 -0300

    feat: Update main.rs to handle dynamic path parameters

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use axum::{routing::get, Router};
+use axum::{extract::Path, routing::get, Router};
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn hello_world(Path(id): Path<String>) -> String {
+    format!("Hello, {id}!")
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new().route("/:id", get(hello_world));
 
     Ok(router.into())
 }
