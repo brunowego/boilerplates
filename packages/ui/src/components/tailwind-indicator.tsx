3330896e68e83cc7563ec9d6e987d44629986421
Bruno Wego <brunowego@gmail.com>
2024-08-06 10:58:30 -0300

    style: Update size classes in UI components

--- packages/ui/src/components/tailwind-indicator.tsx
+++ packages/ui/src/components/tailwind-indicator.tsx
@@ -12,7 +12,7 @@ export default function TailwindIndicator({
   }
 
   return (
-    <div className='fixed right-4 bottom-4 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-foreground p-3 font-semibold text-background text-xs'>
+    <div className='fixed right-4 bottom-4 z-50 flex size-6 items-center justify-center rounded-full bg-foreground p-3 font-semibold text-background text-xs'>
       <div className='block sm:hidden'>xs</div>
       <div className='hidden sm:block md:hidden lg:hidden xl:hidden 2xl:hidden'>
         sm
