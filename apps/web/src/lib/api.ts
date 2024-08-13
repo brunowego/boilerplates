2780c5f718fdd9407edfa770ec0d18536e401090
Bruno Wego <brunowego@gmail.com>
2024-08-12 21:39:31 -0300

    refactor: Simplify base URL in axios configuration

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -5,7 +5,7 @@ import axios, {
 } from 'axios'
 
 const api: AxiosInstance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api`,
+  baseURL: '/api',
 } satisfies AxiosRequestConfig)
 
 export { api as default, axios, isAxiosError }
