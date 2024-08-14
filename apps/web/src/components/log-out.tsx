159bb11f921a5893725d119428a701fd3988f49b
Bruno Wego <brunowego@gmail.com>
2024-08-14 19:06:41 -0300

    refactor: Update LogOut component with loading state

--- apps/web/src/components/log-out.tsx
+++ apps/web/src/components/log-out.tsx
@@ -1,21 +1,30 @@
 'use client'
 
-import type { JSX } from 'react'
+import { type JSX, useState } from 'react'
 
 import Button, { type ButtonProps } from '@acme/ui/components/button'
 import { signOut } from '@acme/auth/react'
-import { LogOut as Icon } from '@acme/ui/components/icon'
+import { Loader2, LogOut as Icon } from '@acme/ui/components/icon'
 
 type LogOutProps = ButtonProps
 
 export default function LogOut({ className }: LogOutProps): JSX.Element {
+  const [isLoading, setIsLoading] = useState(false)
+
   return (
     <Button
       className={className}
-      onClick={() => signOut({ callbackUrl: '/sign-in' })}
-      variant='link'
+      onClick={() => {
+        setIsLoading(true)
+        signOut({ callbackUrl: '/sign-in' })
+      }}
+      variant='ghost'
     >
-      <Icon className='size-5 shrink-0' />
+      {isLoading ? (
+        <Loader2 className='size-5 animate-spin' />
+      ) : (
+        <Icon className='size-5 shrink-0' />
+      )}
 
       <span className='sr-only'>Log Out</span>
     </Button>
