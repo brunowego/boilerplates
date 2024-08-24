7a3e41e82df75310e83729e20f6a9bbe200c41a8
Bruno Gomes <brunowego@gmail.com>
2024-08-24 10:01:40 -0300

    feat: Add POST method to create customer in route

--- apps/web/src/app/api/customers/route.ts
+++ apps/web/src/app/api/customers/route.ts
@@ -19,3 +19,41 @@ export async function GET(): Promise<Response> {
     })
   }
 }
+
+export async function POST(): Promise<Response> {
+  const customersController = new CustomersController(client)
+
+  try {
+    const { result } = await customersController.createCustomer({
+      name: 'Bruno Gomes',
+      email: 'brunowego@gmail.com',
+      document: '01479506192',
+      type: 'individual',
+      address: {
+        street: 'QSB 02',
+        number: '14',
+        zipCode: '72015520',
+        neighborhood: 'Taguatinga',
+        city: 'Taguatinga',
+        state: 'DF',
+        country: 'BR',
+        complement: '',
+        line1: '',
+        line2: '',
+      },
+      metadata: {},
+      phones: {},
+      code: '01J627K5J7S62T36CY52Q5GBGM',
+    })
+
+    return NextResponse.json(result, {
+      status: 200,
+    })
+  } catch (err) {
+    console.error(err)
+
+    return new Response(null, {
+      status: 500,
+    })
+  }
+}
