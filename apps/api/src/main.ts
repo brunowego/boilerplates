235d01fb88d86115fd8df9a2de094488fba5fd44
Bruno Gomes <brunowego@gmail.com>
2024-08-24 13:39:19 -0300

    feat: Add logger to main.ts for server listening message

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import type { NestExpressApplication } from '@nestjs/platform-express'
 import { ConfigService } from '@nestjs/config'
+import { Logger } from '@nestjs/common'
 
 import { AppModule } from './app.module'
+
+const logger = new Logger('Bootstrap')
 
 export async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
@@ -11,7 +14,9 @@ export async function bootstrap() {
   const appHost = configService.get('app.host')
   const appPort = configService.get('app.port')
 
-  await app.listen(appPort, appHost)
+  await app.listen(appPort, appHost, () => {
+    logger.log(`The server is listening on http://${appHost}:${appPort}`)
+  })
 }
 
 bootstrap()
