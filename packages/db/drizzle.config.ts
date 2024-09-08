e2d4c9e15c7f7b51523e01d9633c02f0afb44f57
Bruno Gomes <brunowego@gmail.com>
2024-09-08 09:45:26 -0300

    chore: Add custom migration script

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,9 +1,13 @@
 import type { Config } from 'drizzle-kit'
 
 export default {
+  dialect: 'postgresql',
   out: './src/migrations',
   schema: './src/schema.ts',
-  dialect: 'postgresql',
+  // migrations: {
+  //   table: 'migrations',
+  //   schema: 'public',
+  // },
   dbCredentials: {
     url: process.env.DATABASE_URL as string,
   },
